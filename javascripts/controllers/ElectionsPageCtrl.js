f118206f526f27388b53d4493806435850da4fc4
lewiskg <kenneth.g.lewis2@gmail.com>
2017-12-08 13:05:56 -0600

    API pulls to Goole Maps and Google CIVIC successful.  Need to massage data for display.

--- javascripts/controllers/ElectionsPageCtrl.js
+++ javascripts/controllers/ElectionsPageCtrl.js
@@ -10,9 +10,40 @@ app.controller("ElectionsPageCtrl", function($location, $rootScope, $scope, Poop
 
  	$scope.states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"];
 
+ 	// PoopService.searchCivicElections().then((results) => {
+  //       $scope.blahZip = results.data;
+  //     }).catch((err) => {
+  //       console.log("error in searchCivicElections", err);
+  //     });
+	const getReps = (city, state, zip) => {
+	    PoopService.searchCivicReps(city, state, zip).then((results) => {
+	        $scope.blahReps = results.data;
+	    }).catch((err) => {
+	        console.log("error in searchCivicReps", err);
+	    });
+	};
+
+	 
+	const parseAddress = (cityStateZip) => {
+		let city  = cityStateZip.split(',')[0];
+	  	let state = cityStateZip.split(',')[1].split(" ")[1];
+	  	let zip   = cityStateZip.split(',')[1].split(" ")[2];
+	  	return {"city": city, "state": state, "zip": zip};
+	};
 
 
-
-
+	$scope.runSearch = (zipSearch) => {
+		PoopService.searchByZip(zipSearch).then((results) => {
+	    	let Zip = results.data;
+	    	$scope.blahZip = Zip.results;
+	    	console.log("in zipSearch: ",Zip.results[0].address_components);
+	    	$scope.location = Zip.results[0].formatted_address;
+	    	let address = parseAddress($scope.location);
+	    	getReps(address.city, address.state, address.zip);
+	  	}).catch((err) => {
+	    	console.log("error in runSearch", err);
+	  	});
+	};
+ 	 
 
 });
--- javascripts/controllers/LandingPageCtrl.js
+++ javascripts/controllers/LandingPageCtrl.js
@@ -2,6 +2,56 @@
 
 app.controller("LandingPageCtrl", function($location, $rootScope, $scope, PoopService){
 
+	const initialLoad = () => {
+		PoopService.getCurrentLatLong().then((results) => {
+			let currentPos = results;
+	  		// $scope.latlong = {'latitude': currentPos.coords.latitude, 'longitude': currentPos.coords.longitude};
+	  		getZip(currentPos);
+		}).catch((err) => {
+			console.log("error in initialLoad - getCurrentLocation", err);
+		});
+	};
+
+	initialLoad();
+
+	const getZip = (position) => {
+		PoopService.searchByLatLong(position).then((results) => {
+			let zip = results.data.results[0].address_components[7].short_name;
+			runSearch(zip);
+		}).catch((err) => {
+			console.log("error in getZip", err);
+		});
+	};
+
+	const getReps = (city, state, zip) => {
+	    PoopService.searchCivicReps(city, state, zip).then((results) => {
+	    	$scope.divisions = results.data.divisions;
+	        $scope.offices = results.data.offices;
+	        $scope.officials = results.data.officials;
+	    }).catch((err) => {
+	        console.log("error in searchCivicReps", err);
+	    });
+	};
+	 
+	const parseAddress = (cityStateZip) => {
+		let city  = cityStateZip.split(',')[0];
+	  	let state = cityStateZip.split(',')[1].split(" ")[1];
+	  	let zip   = cityStateZip.split(',')[1].split(" ")[2];
+	  	return {"city": city, "state": state, "zip": zip};
+	};
+
+	const runSearch = (zipSearch) => {
+		PoopService.searchByZip(zipSearch).then((results) => {
+	    	$scope.formatedAddress = results.data.results[0].formatted_address;
+	    	let address = parseAddress($scope.formatedAddress);
+	    	getReps(address.city, address.state, address.zip);
+	  	}).catch((err) => {
+	    	console.log("error in runSearch", err);
+	  	});
+	};
+
+
+	
 
 
 
